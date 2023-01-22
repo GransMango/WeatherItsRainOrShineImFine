f22c871cbb3a08d7ea1b1d626bffbfa902b67ae2
nikno <niknonand@gmail.com>
2023-01-22 20:25:48 +0100

    tried making arduino data a json to sole error in normalizing

--- DataProcessing.py
+++ DataProcessing.py
@@ -42,7 +42,10 @@ normed_x_test = normx(x_test)
 normed_y_train = normy(y_train)
 normed_y_test = normy(y_test)
 
-arduino_da = pd.read_json(directory + '\\Data\\arduino_data.json')
+with open(directory + '\\Data\\arduino_data.json', "r") as file:
+    a_data = json.loads(file.read())
+
+arduino_da = pd.DataFrame.from_dict(a_data)
 arduino_features = ['temp', 'humidity', 'pressure', 'rain', 'windspeed', 'winddeg', 'month', 'day', 'hour']
 arduino_data = pd.DataFrame(arduino_da, columns=arduino_features)
 normed_arduino_data_raw = normx(arduino_da)
