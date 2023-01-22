80e637e599fa43a09ae2806c52f6031c7436d0de
Daniel <GransMango@github.com>
2023-01-22 19:15:45 +0100

    Changed main file to include args

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import argparse
 parser = argparse.ArgumentParser(description='Deep learning weather program')
-parser.add_argument('out', help='selects output for files (optional)')
-parser.add_argument('in', help='selects directory containing model (optional)')
-parser.add_argument('buildModel')
 
-def main
+parser.add_argument('-out', help='selects output for files (optional)')
+parser.add_argument('-in', help='selects directory containing model (optional)')
 
+parser.add_argument('-fetch', help='fetch api data', required=False)
+parser.add_argument('--query', help='user location, format: city, country', default='Oslo, Norge', required=False)
+parser.add_argument('-build', help='build model if you dont have yet', required=False)
+parser.add_argument('-predict', help='runs model', required=False)
 
+args = parser.parse_args()
 
+print(args.query)
+#api
 
+#dataprocessing
+#buildmodel
 
-if __name__ == "__main__":
+#model
+
+# def main
+#
+#
+#
+#
+#
+# if __name__ == "__main__":
